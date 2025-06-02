734c6bd77fc3c1f06210ec7733bca46ed4d85d49
Adam Lyon <adam@nullrider.com>
2025-06-02 06:42:54 -0600

    Added dom to playground

--- 00-playground/dom/script.js
+++ 00-playground/dom/script.js
@@ -1,8 +1,26 @@
-// your JavaScript file
-const container = document.querySelector("#container");
+const link = document.querySelector("a");
 
-const content = document.createElement("div");
-content.classList.add("content");
-content.textContent = "This is the glorious text-content!";
+link.textContent = "Mozilla Developer Network";
+link.href = "https://developer.mozilla.org";
 
-container.appendChild(content);
+const sect = document.querySelector("section");
+
+const para = document.createElement("p");
+para.textContent = "We hope you enjoy the ride!";
+
+sect.appendChild(para);
+
+const text = document.createTextNode(
+    " - the premier source for web development knowledge.",
+);
+
+const linkPara = document.querySelector("p");
+linkPara.appendChild(text);
+
+
+text.remove();
+
+para.setAttribute("class", "highlight");
+
+
+
